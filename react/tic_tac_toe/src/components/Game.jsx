ff3781ca979365ae8df830fba15ba2eec9a88c38
Li Yi <jayceeyili@gmail.com>
2017-09-04 21:40:19 -0700

    seprate components based on functionality

---
+++ react/tic_tac_toe/src/components/Game.jsx
@@ -0,0 +1,18 @@
+import Board from './Board.jsx';
+import React, { Component } from 'react';
+
+export default class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
