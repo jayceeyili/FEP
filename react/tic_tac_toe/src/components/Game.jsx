22f3c37ce58771b7645bc974cbc68d63dbfba7d5
Li Yi <jayceeyili@gmail.com>
2017-09-10 00:42:04 -0700

    finish all functionality in tutorial

--- react/tic_tac_toe/src/components/Game.jsx
+++ react/tic_tac_toe/src/components/Game.jsx
@@ -11,12 +11,13 @@ export default class Game extends Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true
     }
   }
 
   handleClick(i) {
-    let history = this.state.history;
+    let history = this.state.history.slice(0, this.state.stepNumber + 1);
     let current = history[history.length - 1];
     let winner = calculateWinner(current.squares);
     let squares = current.squares.slice();
@@ -28,16 +29,38 @@ export default class Game extends Component {
       history: [...this.state.history, ...[{
         squares: squares
       }]],
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    })
+  }
+
+  jumpTo(i) {
+    this.setState({
+      stepNumber: i,
+      xIsNext: i % 2 === 0
     })
   }
 
   render() {
     let history = this.state.history;
-    let current = history[history.length - 1];
+    let current = history[this.state.stepNumber];
     let winner = calculateWinner(current.squares);
     let next = this.state.xIsNext ? 'X' : 'O';
     const status = winner ? `Winner: ${winner}` : `Next player: ${next}`;
+
+    const moves = history.map((step, i) => {
+      const desc = i ? `Move #${i}` : 'Game Start';
+      return (
+        <li key={ i }>
+          <a
+            href="#"
+            onClick={ () => this.jumpTo(i) }
+          >
+            { desc }
+          </a>
+        </li>
+      )
+    });
 
     return (
       <div className="game">
@@ -49,7 +72,7 @@ export default class Game extends Component {
         </div>
         <div className="game-info">
           <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
