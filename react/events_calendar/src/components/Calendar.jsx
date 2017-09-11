4984ae25ee7120c2e3ed99b775f3d68eceaceab3
Li Yi <jayceeyili@gmail.com>
2017-09-11 13:16:57 -0700

    change Calendar to Classical component

--- react/events_calendar/src/components/Calendar.jsx
+++ react/events_calendar/src/components/Calendar.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Calendar = props => {
+// const Calendar = props => {
+//
+//   return (
+//     <div>
+//       This is the Calendar
+//     </div>
+//   )
+// };
+//
+// export default Calendar;
+export default class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    const date = new Date();
 
-  return (
-    <div>
-      This is the Calendar
-    </div>
-  )
-};
+    this.state = {
+      year: date.getFullYear(),
+      month: date.getMonth()
+    }
+  }
 
-export default Calendar;
+  render() {
+    return (
+      <div>{ this.state.year + ' ' + this.state.month }</div>
+    )
+  }
+}
