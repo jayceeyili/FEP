39f6d246b3fd9e3b7deaff4c0c16cffd0a4c4811
Li Yi <jayceeyili@gmail.com>
2017-09-11 15:34:44 -0700

    add some new states in Calendar

--- react/events_calendar/src/components/Calendar.jsx
+++ react/events_calendar/src/components/Calendar.jsx
@@ -7,13 +7,26 @@ export default class Calendar extends Component {
 
     this.state = {
       year: date.getFullYear(),
-      month: date.getMonth()
+      month: date.getMonth(),
+      selectedYear: date.getFullYear(),
+      selectedMonth: date.getMonth(),
+      selectedDate: date.getDate(),
+      selectedDt: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+      startDay: 1,
+      weekNumbers: false,
+      dayNames: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+      monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      monthNamesFull: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      firstMonth: null,
+      daysInMonth: null,
+      minDate: null,
+      disablePast: false
     }
   }
 
   render() {
     return (
-      <div>{ this.state.year + ' ' + this.state.month }</div>
+      <div>{ this.state.selectedMonth }</div>
     )
   }
 }
