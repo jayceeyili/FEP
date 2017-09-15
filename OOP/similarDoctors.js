99c5adbb13d0706d1842d9d705f1673d3676bad0
Li Yi <jayceeyili@gmail.com>
2017-09-15 12:48:05 -0700

    add description for similar doctors

---
+++ OOP/similarDoctors.js
@@ -0,0 +1,10 @@
+/***************************************************************************************************
+  A site contains a listing of doctors. Users can browse for docutors given a specific specialty,
+  area, review score etc. Write a function which when given a doctor, provides a list of similiar
+  doctors, in a prioritized order. You define what similar means and the result ordering, but clearly
+  document any assumptions in your code. You can assume the entire directory of doctors fits into
+  memory, and write in JavaScript. Include unit tests.
+
+  Aim to spend a maximum of 45 minutes on this coding challenge. If applicable, note down any remaing
+  thoughts or things you would want to improve on with more time.
+***************************************************************************************************/
