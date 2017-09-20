4a88c16f36c5fdd2013f96275d6b45cb80543db4
Li Yi <jayceeyili@gmail.com>
2017-09-20 01:32:30 -0700

    second function: fizzBuzz pow of 2

--- JS/codingChallenge.js
+++ JS/codingChallenge.js
@@ -7,13 +7,25 @@ const fizzBuzz_2_3 = (start, end) => {
     console.log((f ^ b) ? i : '');
   }
 };
-fizzBuzz_2_3(1, 20);
+// fizzBuzz_2_3(1, 20);
+
 // b) same requirement except print number that is power of 2
 
 const fizzBuzz_p2 = (start, end) => {
-  
+  for (let i = start; i <= Math.sqrt(end); i++) {
+    if (Math.pow(2, i) <= end) console.log(Math.pow(2, i));
+  }
 };
+// fizzBuzz_p2(1, 100);
+
 // c) Reverse a string
+// const reverse = (string, str = string.split('')) => {
+//   for (let i = 0; i <= Math.floor(str.length / 2); i++) {
+//     [str[i], str[str.length - 1 - i]] = [str[str.length - 1 - i], str[i]];
+//   }
+//   return str.join('');
+// };
+// console.log(reverse('hello'));
 // d) write a function to increment version # string in the format of <major>>.<minor>.<feature>.<patch> with version as input in STRING…
 //
 // What can you bring to our scrappy culture
