54e3cbb27b9e237011676f12234e8f9d0ac73cce
Li Yi <jayceeyili@gmail.com>
2017-09-19 23:52:28 -0700

    build the first function

--- JS/codingChallenge.js
+++ JS/codingChallenge.js
@@ -1,10 +1,10 @@
-a) write a function to take 2 variables as number range. print only numbers that are divisible by 2 & 3
-b) same requirement except print number that is power of 2
-c) Reverse a string
-d) write a function to increment version # string in the format of <major>>.<minor>.<feature>.<patch> with version as input in STRING…
-
-What can you bring to our scrappy culture
-Describe a time you had to convince others to adopt your idea.
-
-
-I was asked about <dd>, <dl> and <dt> tags which were something that had dropped out of my brain a while back.
+// a) write a function to take 2 variables as number range. print only numbers that are divisible by 2 & 3
+// b) same requirement except print number that is power of 2
+// c) Reverse a string
+// d) write a function to increment version # string in the format of <major>>.<minor>.<feature>.<patch> with version as input in STRING…
+//
+// What can you bring to our scrappy culture
+// Describe a time you had to convince others to adopt your idea.
+//
+//
+// I was asked about <dd>, <dl> and <dt> tags which were something that had dropped out of my brain a while back.
