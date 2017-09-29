c3fc5703d72022d3d626c16052a39b1275372e8b
Li Yi <jayceeyili@gmail.com>
2017-09-29 14:28:52 -0700

    add descriptions

--- JS/alison_onsite.js
+++ JS/alison_onsite.js
@@ -1,3 +1,15 @@
 /***************************************************************************************************
+  Given an integer input representing phone numbers, generate all the combinations.
+  {0: ‘’,
+  1: ‘’,
+  2:’abc’,
+  ……}
+
+  next Level: what if you already have a dictionaray and you only want words that exist in it?
+***************************************************************************************************/
+
+/***************************************************************************************************
+  input: text(type: space separated words in string format), limit(integer),
+  find the top limit number of words in the text, return the result in an array, use O(n) time.
 
 ***************************************************************************************************/
