fe1e63353a03fc90d393c49d91c79748f628047f
Li Yi <jayceeyili@gmail.com>
2017-09-29 21:25:45 -0700

    keep working later

--- JS/alison_onsite.js
+++ JS/alison_onsite.js
@@ -8,6 +8,8 @@
   next Level: what if you already have a dictionaray and you only want words that exist in it?
 ***************************************************************************************************/
 
+const
+
 /***************************************************************************************************
   input: text(type: space separated words in string format), limit(integer),
   find the top limit number of words in the text, return the result in an array, use O(n) time.
