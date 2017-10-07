f77dad2e0756662890bf202ad88e3604979754d9
Li Yi <jayceeyili@gmail.com>
2017-10-07 08:04:17 -0700

    add questions got from jg

---
+++ JS/jg_interviewQustions.js
@@ -0,0 +1,21 @@
+/***************************************************************************************************
+  Given an array of positive integers and a target number, return true if a sum of contiguous values
+  within the array matches the target number (Facebook)
+***************************************************************************************************/
+
+
+
+
+
+/***************************************************************************************************
+  1, Given a list of characters, write a function that outputs one palindrome that can be made using
+  ALL of the input characters once
+  2, Output all palindrome combinations (Uber)
+***************************************************************************************************/
+
+
+/***************************************************************************************************
+  implement: firstServer(serverArray) {}
+  input: serverArray is an array of integer server numbers (all different) > 0
+  Return the server ID of the first positive integer not in the list (Stripe)
+***************************************************************************************************/
