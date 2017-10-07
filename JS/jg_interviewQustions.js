7a8efd0664e8289e13b070bc8f51709e5c516189
Li Yi <jayceeyili@gmail.com>
2017-10-07 08:47:35 -0700

    naive solotion of matchTarget

--- JS/jg_interviewQustions.js
+++ JS/jg_interviewQustions.js
@@ -3,9 +3,26 @@
   within the array matches the target number (Facebook)
 ***************************************************************************************************/
 
+const matchTarget = (array, target) => {
+  // naive solotion
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i] === target) return true;
+  //   let sum = array[i];
+  //   for (let k = i + 1; k < array.length; k++) {
+  //     let curr = array[k];
+  //     sum += curr;
+  //     if (sum === target) return true;
+  //   }
+  // }
+  //
+  // return false;
 
+  
+}
 
-
+let a = [1, 11, 2, 4, 5, 7];
+let t = 30;
+console.log(matchTarget(a, t));
 
 /***************************************************************************************************
   1, Given a list of characters, write a function that outputs one palindrome that can be made using
